94297743c48538fe5b3446ef439d0dbd070d9a4a
IrPedro <irpedrogsp@usp.br>
2026-03-24 01:21:38 -0300

    feat: cria dicionario cientifico (seed), camada gold com regras de negocio e atualiza documentacao

---
+++ plant_sensor_dbt/models/Gold/monitorizacao_plantas_gold.sql
@@ -0,0 +1,63 @@
+{{ config(materialized='view') }}
+
+WITH leituras AS (
+    SELECT * FROM {{ ref('vw_leituras_silver') }}
+),
+
+limites_cientificos AS (
+    SELECT * FROM {{ ref('limites_plantas_cientifico') }}
+),
+
+cruzamento AS (
+    SELECT 
+        l.id_leitura,
+        l.data_leitura_br,
+        l.temperatura_c,
+        l.umidade_ar_pct,    
+        l.umidade_solo_pct,
+        -- l.luminosidade,      <-- COMENTADO: Aguardando calibração do hardware
+        c.nome_popular AS planta_monitorizada,
+        c.temp_min_c,
+        c.temp_max_c,
+        c.umid_ar_min_pct,
+        c.umid_ar_max_pct,
+        -- c.lux_min,           <-- COMENTADO: Aguardando Master Data de luz
+        c.tolerancia_seca,
+        TRUE AS flg_origem_dados_confiavel 
+    FROM leituras l
+    LEFT JOIN limites_cientificos c 
+        ON c.nome_popular = 'Zamioculca'
+)
+
+SELECT 
+    *,
+    -- 1. Temperatura
+    CASE 
+        WHEN temperatura_c < temp_min_c THEN 'ALERTA: Frio extremo'
+        WHEN temperatura_c > temp_max_c THEN 'ALERTA: Calor extremo'
+        ELSE 'Temperatura Ideal'
+    END AS status_temperatura,
+    
+    -- 2. Umidade do Ar
+    CASE 
+        WHEN umidade_ar_pct < umid_ar_min_pct THEN 'ALERTA: Ar muito seco'
+        WHEN umidade_ar_pct > umid_ar_max_pct THEN 'ALERTA: Ar muito úmido'
+        ELSE 'Umidade do Ar Adequada'
+    END AS status_umidade_ar,
+
+    -- 3. Umidade do Solo
+    CASE 
+        WHEN umidade_solo_pct < 20 THEN 'ALERTA: Solo muito seco (Regar!)'
+        WHEN umidade_solo_pct > 80 THEN 'ALERTA: Solo encharcado'
+        ELSE 'Umidade do Solo Adequada'
+    END AS status_umidade_solo
+
+    -- 4. Luminosidade 
+    /* <-- Bloco inteiro comentado para uso futuro no Passo 14
+    , CASE 
+        WHEN luminosidade < lux_min THEN 'ALERTA: Luz insuficiente'
+        ELSE 'Luminosidade Adequada'
+    END AS status_luminosidade
+    */
+
+FROM cruzamento
